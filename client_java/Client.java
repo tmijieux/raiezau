f5a0d608c16ee3598775e9706c024fcbdb68b4ef
Maugere Lucas <lmaugere@enseirb-matmeca.fr>
2016-02-09 18:02:44 +0100

    Début du client en java

---
+++ client_java/Client.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.net.*;
+
+class Client {
+    public static void main(String args[]) throws Exception {
+	P2TProtocole p2t = new P2TProtocole();
+	boolean r = p2t.announce();
+	System.out.println("announce: " + r);
+    }
+}
---
+++ client_java/P2TProtocole.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.net.*;
+
+class P2TProtocole 
+{
+    private int port = 8080;
+    private String ip = "localhost";
+
+    private Socket socket;
+    private BufferedReader reader;
+    private PrintWriter writer;
+
+    P2TProtocole() 
+	throws Exception 
+    {
+	socket = new Socket(ip, port);
+	reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	writer = 
+	    new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+    }
+
+    private void seed(String filename, int length, String key)
+    {
+	writer.print("seed ");
+	writer.print(filename + " " + length + " " + key + " ");	
+    }
+
+    private void leech(String key)
+    {
+	writer.print("seed ");
+	writer.print(key + " ");	
+    }
+
+    boolean announce() 
+	 throws Exception 
+    {
+	writer.print("announce listen " + port + " ");
+	seed("file1", 16, "key1");
+	leech("key2");
+	writer.println("");
+
+	String response = reader.readLine();
+
+	if (response.compareTo("OK") != 0)
+	    return false;
+	return true;
+    }
+
+}
