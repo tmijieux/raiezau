d9d8374d84c963a787cbc069ef6298154580dec5
Thomas Mijieux <thomas.mijieux@hotmail.fr>
2016-04-27 14:14:49 +0200

    locked hash_table

--- cutil/hash_table.h
+++ cutil/hash_table.h
@@ -1,19 +1,3 @@
-/*
- *  Copyright (©) 2015 Lucas Maugère, Thomas Mijieux
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
 #ifndef HASH_TABLE_H
 #define HASH_TABLE_H
 
@@ -31,6 +15,6 @@ int ht_hash(struct hash_table *ht, const char *key);
 void ht_free(struct hash_table* ht);
 void ht_for_each(struct hash_table* ht,
 		 void (*fun)(const char *, void*, void*), void *args);
-struct list* ht_to_list(const struct hash_table *ht);
+struct list* ht_to_list(struct hash_table *ht);
 
 #endif //HASH_TABLE_H
